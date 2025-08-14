11aa5f2187de589849e6c132253c24298fbb2f71
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-14 22:33:28 +0100

    Add CSS reset, table element, and JavaScript DOM manipulation

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 const get=document.documentElement
+let el=table.row[0].cells[1]
+for(let i=0;i<document.body.childNodes.length;i++){
+    alert(document.body.childNodes[i])
+}
+// for (let node of document.body.childNodes) {
+//   alert(node); // shows all nodes from the collection
+// }
 
+el.style.background='red';
